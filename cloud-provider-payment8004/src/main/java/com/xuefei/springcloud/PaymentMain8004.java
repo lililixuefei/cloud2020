5aa773e3e2b38a539a4dc2deb63f5d90478663cc
xuefei <13273726905@163.com>
2020-09-11 00:11:32 +0800

    【服务注册进Zookeeper】--李雪飞--2020年09月11日00:11:25

---
+++ cloud-provider-payment8004/src/main/java/com/xuefei/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.xuefei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2020/09/09 22:12
+ */
+@SpringBootApplication
+@EnableDiscoveryClient    // 该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/xuefei/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.xuefei.springcloud.controller;
+
+import cn.hutool.core.lang.UUID;
+import com.xuefei.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2020/09/03 00:29
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/zk")
+    public String create() {
+        return "springcloud with zookeeper:" + serverPort + " \t " + UUID.randomUUID().toString();
+    }
+}
